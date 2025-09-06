96cf8bcafdc7f7dfb8fced4fa8df673bd999a78a
WorkMaster0 <stasvit058@gmail.com>
2025-09-06 15:06:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import time
 # -------------------------
 # Налаштування
 # -------------------------
-API_KEY_TELEGRAM = "YOUR_TELEGRAM_BOT_TOKEN"
-CHAT_ID = "YOUR_CHAT_ID"
+API_KEY_TELEGRAM = "8051222216:AAFORHEn1IjWllQyPp8W_1OY3gVxcBNVvZI"
+CHAT_ID = "6053907025"
 TIMEFRAMES = ["5m", "15m", "1h", "4h"]
 N_CANDLES = 30
 FAST_EMA = 10
